1ad81a9dbb42df1782ed506c87eaa3eeaf1389f5
Student <emilywright212@gmail.com>
2020-01-31 15:43:20 +0000

    Added game credit

--- lib/fruit_machine.rb
+++ lib/fruit_machine.rb
@@ -1,5 +1,20 @@
 class FruitMachine
-  def display
-    ['black', 'white', 'yellow']
+
+  SINGLE_PLAY_FEE = 5
+
+  attr_reader :game_credit
+
+  def initialize
+    @slots = ['black', 'white', 'green', 'yellow']
+    @game_credit = 0
   end
+
+  def display
+    @slots.sample(4).join(" | ")
+  end
+
+  def insert(money)
+    @game_credit += money / SINGLE_PLAY_FEE 
+  end
+
 end
--- spec/fruit_machine_spec.rb
+++ spec/fruit_machine_spec.rb
@@ -2,7 +2,16 @@ require 'fruit_machine'
 describe FruitMachine do
   describe '#display' do
     it 'will display slots' do
-      expect(subject.display).to eq ['black', 'white', 'yellow']
+      allow(subject).to receive(:display).and_return("black | white | green | yellow")
+      expect(subject.display).to eq "black | white | green | yellow"
+    end
+    it 'randomly displays slots with colours' do
+      allow(subject).to receive(:display).and_return("yellow | yellow | yellow | yellow")
+      expect(subject.display).to eq "yellow | yellow | yellow | yellow"
+    end
+    it 'gives a game credit for every £five inserted' do
+      subject.insert(5)
+      expect(subject.game_credit).to eq(1)
     end
   end
 end
